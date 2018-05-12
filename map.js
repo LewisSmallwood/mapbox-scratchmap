0ee68f051d1791e055da244ffdfe4e078f1392c5
Lewis Smallwood <lewis@smallwood.it>
2018-05-12 21:10:50 +0100

    Map countries I have visited.

--- map.js
+++ map.js
@@ -10,10 +10,65 @@ mapboxgl.accessToken = getAccessToken();
 // Define a map centered on Europe.
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v9', // stylesheet location
+    style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
     center: [20, 52.4862], // starting position [lng, lat]
     zoom: 3 // starting zoom
 });
+
+map.on('load', function () {
+
+    // Lookup the order of text layers so we place the selected country layers behind the text.
+    var layers = map.getStyle().layers;
+    var firstSymbolId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+            firstSymbolId = layers[i].id;
+            break;
+        }
+    }
+
+    // Define a GeoJSON source of all countries and their boundaries.
+    map.addSource('countries', {
+        "type": "geojson",
+        "data": './data/countries.json'
+    });
+
+    // Get the countries I have visited and add them to a new map layer.
+    httpGETRequest("./visited.json", function(countries) {
+
+        countries = JSON.parse(countries);
+
+        map.addLayer({
+            'id': 'country',
+            'type': 'fill',
+            'source': 'countries',
+            'layout': {
+            },
+            'paint': {
+                'fill-opacity': 0.8,
+                'fill-color': 'rgba(200, 100, 240, 0.4)',
+                'fill-outline-color': 'rgba(200, 100, 240, 1)'
+            },
+            "filter": buildFilter(countries["visited"])
+        }, firstSymbolId);
+
+    });
+});
+
+// Construct the search filter.
+function buildFilter(arr) {
+    var filter = ['in', 'name_long'];
+
+    if (arr.length === 0) {
+        return filter;
+    }
+
+    for(var i = 0; i < arr.length; i += 1) {
+        filter.push(arr[i]);
+    }
+
+    return filter;
+}
 
 // In order for the example to function without a hardcoded access token in the repository, your access token is
 // requested using a prompt and then stored in LocalStorage for returning sessions. This function will get your saved
@@ -44,4 +99,14 @@ function getAccessToken() {
     }
 
     return accessToken;
+}
+
+function httpGETRequest(requestURL, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", requestURL, true); // true for asynchronous
+    xmlHttp.send(null);
 }
